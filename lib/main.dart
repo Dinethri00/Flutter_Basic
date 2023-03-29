96951794ddd3dd2988e10ad9f09352e8fd590874
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-03-29 23:17:31 +0530

    Rows And Columns

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,39 @@ void main() {
       debugShowCheckedModeBanner:false,
       home: Scaffold  (
           backgroundColor: Colors.teal,
-          body: Container(),
+          body: SafeArea(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+                SizedBox(height: 20.0, width: 20.0),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.amber,
+                  child: Text('Container 2'),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.deepPurple,
+                  child: Text('Container 3'),
+                ),
+
+
+              ],
+            ),
+
+          ),
         ),
       );
     }
   }
-  
+
 
 
