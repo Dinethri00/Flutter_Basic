1e61f4bbbc5c770773d21147d685f955e2671368
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-05-20 23:12:13 +0530

    QR Test

--- lib/main.dart
+++ lib/main.dart
@@ -404,64 +404,214 @@
 //   }
 // }
 //
-import 'dart:async';
-
+// import 'dart:async';
+//
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Flutter Demo',
+//       home: MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   DateTime selectedDate = DateTime.now();
+//
+//   Future<void> _selectDate(BuildContext context) async {
+//     final DateTime? picked = await showDatePicker(
+//         context: context,
+//         initialDate: selectedDate,
+//         firstDate: DateTime(2015, 8),
+//         lastDate: DateTime(2101));
+//     if (picked != null && picked != selectedDate) {
+//       setState(() {
+//         selectedDate = picked;
+//       });
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: <Widget>[
+//             Text("${selectedDate.toLocal()}".split(' ')[0]),
+//             const SizedBox(height: 20.0,),
+//             ElevatedButton(
+//               onPressed: () => _selectDate(context),
+//               child: const Text('Select date'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr/src/qr_image.dart';
+import 'package:qr_image/qr_image.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class GenerateQrCodePage extends StatefulWidget {
+  const GenerateQrCodePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GenerateQrCodePage> createState() => _GenerateQrCodePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  DateTime selectedDate = DateTime.now();
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
+class _GenerateQrCodePageState extends State<GenerateQrCodePage> {
+  String qrData = 'https://www.github.com/kamranhccp';
+  final qrText = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'QR Code Generator',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("${selectedDate.toLocal()}".split(' ')[0]),
-            const SizedBox(height: 20.0,),
-            ElevatedButton(
-              onPressed: () => _selectDate(context),
-              child: const Text('Select date'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text("Generated QR Code",
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black),
+                  textAlign: TextAlign.center),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 250,
+                  child: QrImage(
+                    data: qrData,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Divider(
+                height: 20.0,
+                thickness: 1.0,
+                color: Colors.black,
+                endIndent: 42.0,
+                indent: 42.0,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+                child: const Text(
+                  "You can also generate your own QR Code by entering the text below",
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextField(
+                controller: qrText,
+                decoration: const InputDecoration(
+                  hintText: "Enter your Data",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        16.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 14.0,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 55.0, right: 55.0),
+                height: 50.0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.black,
+                        width: 1.0,
+                        style: BorderStyle.solid,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        32.0,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (qrText.text.isEmpty) {
+                      setState(() {
+                        qrData = "https://www.github.com/kamranhccp";
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                              "Please enter some Data!",
+                              style: TextStyle(fontSize: 17.0),
+                            ),
+                          ),
+                        );
+                      });
+                    } else {
+                      setState(() {
+                        qrData = qrText.text;
+                      });
+                    }
+                  },
+                  child: const Text(
+                    "Generate QR Code",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
