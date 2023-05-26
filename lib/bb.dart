6bf587bde7a28033bd4da8353cb87d362f4a1396
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-05-26 20:06:09 +0530

    Testing2

--- lib/bb.dart
+++ lib/bb.dart
@@ -7,48 +7,48 @@ class SalaryPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Salary Page'),
       ),
-      // body: Center(
-      //   child: Container(
-      //     margin: EdgeInsets.all(20.0),
-      //     child: Stack(
-      //       children: [
-      //         Align(
-      //           alignment: Alignment.topLeft,
-      //           child: Container(
-      //             padding: EdgeInsets.all(10.0),
-      //             decoration: BoxDecoration(
-      //               border: Border.all(color: Colors.black),
-      //             ),
-      //             child: Text(
-      //               'Month Salary',
-      //               style: TextStyle(
-      //                 fontSize: 18.0,
-      //                 fontWeight: FontWeight.bold,
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //         Align(
-      //           alignment: Alignment.topRight,
-      //           child: Container(
-      //             padding: EdgeInsets.all(10.0),
-      //             decoration: BoxDecoration(
-      //               color: Colors.grey[300],
-      //               border: Border.all(color: Colors.black),
-      //             ),
-      //             child: Text(
-      //               'Generated Salary',
-      //               style: TextStyle(
-      //                 fontSize: 18.0,
-      //                 fontWeight: FontWeight.bold,
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: Text(
+                    'Month Salary',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: Text(
+                    'Generated Salary',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
