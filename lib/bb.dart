2f4bd6d48b8ef1f9e22c16ecedf3a64b763616bf
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-10 22:12:43 +0530

    Update bb.dart

--- lib/bb.dart
+++ lib/bb.dart
@@ -1,54 +1,54 @@
-import 'package:flutter/material.dart';
-
-class SalaryPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Salary Page'),
-      ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.all(20.0),
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Container(
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Text(
-                    'Month Salary',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Text(
-                    'Generated Salary',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class SalaryPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Salary Page'),
+//       ),
+//       body: Center(
+//         child: Container(
+//           margin: EdgeInsets.all(20.0),
+//           child: Stack(
+//             children: [
+//               Align(
+//                 alignment: Alignment.topLeft,
+//                 child: Container(
+//                   padding: EdgeInsets.all(10.0),
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.black),
+//                   ),
+//                   child: Text(
+//                     'Month Salary',
+//                     style: TextStyle(
+//                       fontSize: 18.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               Align(
+//                 alignment: Alignment.topRight,
+//                 child: Container(
+//                   padding: EdgeInsets.all(10.0),
+//                   decoration: BoxDecoration(
+//                     color: Colors.grey[300],
+//                     border: Border.all(color: Colors.black),
+//                   ),
+//                   child: Text(
+//                     'Generated Salary',
+//                     style: TextStyle(
+//                       fontSize: 18.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
